bc660b8c57fcc25a299f20212c9743922e47260f
Sandeep Das <sandeep27d@gmail.com>
2025-04-06 17:39:23 +0530

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Stopwatch from "./components/stopwatch";
+
 import { Countdown } from "./components/Countdown";
+import Stopwatch from "./components/stopwatch";
 
 function App() {
   const [count, setCount] = useState(0);
